7e919b5b8a0bc17e3a215e5334cc5ed46f6b6786
Matt Griffiths <matthew.griffiths2@gmail.com>
2017-04-29 17:09:43 +1000

    * Added jinja2 templating engine * Added class based views * Added pyramid_jinja2 to setup

--- stagger/views.py
+++ stagger/views.py
@@ -1,14 +1,11 @@
-from pyramid.response import Response
-from pyramid.view import view_config
+from pyramid.view import view_config, view_defaults
 
 
-# First view, available at http://localhost:6543/
-@view_config(route_name='home')
-def home(request):
-    return Response('<body>Visit <a href="/howdy">hello</a></body>')
+@view_defaults(renderer='templates\home.jinja2')
+class TutorialViews:
+    def __init__(self, request):
+        self.request = request
 
-
-# /howdy
-@view_config(route_name='hello')
-def hello(request):
-    return Response('<body>Go back <a href="/">home</a></body>')
+    @view_config(route_name='home')
+    def home(self):
+        return {'name': 'Home'}
